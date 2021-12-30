0a34cf5010301651784c24764eea5fbd0f74df8c
orifjon <orifjon.5000@gmail.com>
2021-12-30 22:00:19 +0900

    add fetch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 export default function App() {
-    const register =async ()=>{
-        try {
-const res =fetch('https://reqres.in/api/register').then(res=>res.json)
-        }
-    }catch(error){
 
+  const handleInputChange =(e)=>{
+    
+  }
+  const registerUser = async () => {
+    try {
+      //   const res = await fetch("https://reqres.in/api/register");
+      //   const data = await res.json();
+      const data = axios.post("https://reqres.in/api/register", {});
+      console.log(data);
+    } catch (error) {
+      console.log(error);
     }
-    return (
-        <div>
-            <h2>USer List</h2>
-        </div>
-    )
+  };
+  return (
+    <div>
+      <h2>USer List</h2>
+      <form action="">
+        <p>
+          <input type="email" name="email" required onChange={handleChange} />
+        </p>
+        <p>
+          <input
+            type="password"
+            name="password"
+            required
+            onChange={handleInputChange}
+          />
+        </p>{" "}
+      </form>
+      <button onClick={registerUser}>Login</button>
+    </div>
+  );
 }
