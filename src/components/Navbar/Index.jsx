5117685426b4ffa0db7054148e6cf1fe71e560df
orifjon <orifjon.5000@gmail.com>
2022-01-10 14:38:52 +0900

    add dd

--- src/components/Navbar/Index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Container } from "./Style";
-
-export default function Index() {
-  return <Container>sd</Container>;
-}
--- src/components/Navbar/Style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-width:100%;
-height: 30px;
-background: blue;
-`
